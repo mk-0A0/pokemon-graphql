c6919ca7969d2b0259b01e185db1c5df62a23273
momoka <moowfaw@gmail.com>
2022-05-05 00:24:21 +0900

    ✨initialize apollo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'https://graphql-pokemon2.vercel.app/',
+  // キャッシュを参照しにに行く
+  cache: new InMemoryCache()
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+  <ApolloProvider client={client}>
+  <Component {...pageProps} />
+  </ApolloProvider>
+  )
 }
 
 export default MyApp
