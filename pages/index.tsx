979a01cc2f1755083fbb5ed6d3d50d438f9f8341
momoka <moowfaw@gmail.com>
2022-05-05 01:25:43 +0900

    🎨一覧スタイリング

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type {NextPage} from 'next'
 import {useQuery} from "@apollo/client";
 import {PokemonsData, PokemonsQuery} from "../graphql/queries/pokemons.query";
+import {Box, Text, Image, Flex, Grid, Container} from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   const {data} = useQuery<PokemonsData>(PokemonsQuery, {
@@ -10,11 +11,19 @@ const Home: NextPage = () => {
   })
 
   return (
-    <>
-      {data?.pokemons.map((pokemon) => (
-        <p key={pokemon.id}>{pokemon.name}</p>
-      ))}
-    </>
+    <Container maxW={1000} w={"full"} py={20}>
+      <Grid templateColumns={'repeat(3, 1fr)'} gap={10}>
+        {data?.pokemons.map((pokemon) => (
+          <Flex alignItems={"center"} borderWidth={1} gap={5} p={4} key={pokemon.id}>
+            <Text>{pokemon.number}</Text>
+            <Box maxW={50} w={"full"} flexShrink={0}>
+              <Image src={pokemon.image}/>
+            </Box>
+            <Text>{pokemon.name}</Text>
+          </Flex>
+        ))}
+      </Grid>
+    </Container>
   )
 }
 
